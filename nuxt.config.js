7cb0d7f3d84565864337b80f45162c951e92d743
Kenny Heinonen <kenny_heinonen@hotmail.com>
2021-09-03 22:04:10 +0300

    Add GA

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module',
+    '@nuxtjs/google-analytics',
     'nuxt-purgecss'
   ],
 
@@ -56,6 +57,10 @@ export default {
   // Sitemap
   sitemap: {
     hostname: 'https://koulutettuninja.fi'
+  },
+
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS_ID
   },
 
   // PurgeCSS
