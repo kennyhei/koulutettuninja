6cef2b943b5bb732cdd8a24037bde9a7838297ee
Kenny Heinonen <kenny_heinonen@hotmail.com>
2021-12-21 23:21:25 +0200

    Style

--- components/VHero.vue
+++ components/VHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero ninja-bg is-relative" style="position: relative;">
+  <section class="hero ninja-bg is-relative">
 
   <div class="snowflake">
   ❅
@@ -138,35 +138,35 @@ export default {
 
 @-webkit-keyframes snowflakes-shake {
   0% {
-    -webkit-transform:translateX(0px);
-    transform:translateX(0px);
+    -webkit-transform: translateX(0px);
+    transform: translateX(0px);
   }
   50% {
-    -webkit-transform:translateX(60px);
-    transform:translateX(60px);
+    -webkit-transform: translateX(60px);
+    transform: translateX(60px);
   }
   100% {
-    -webkit-transform:translateX(0px);
-    transform:translateX(0px);
+    -webkit-transform: translateX(0px);
+    transform: translateX(0px);
   }
 }
 @keyframes snowflakes-fall {
   0%{
-    top:-15%;
+    top: -60%;
   }
   100% {
-    top:90%;
+    top: 90%;
   }
 }
 @keyframes snowflakes-shake {
   0% {
-    transform:translateX(0px);
+    transform: translateX(0px);
   }
   50% {
-    transform:translateX(60px);
+    transform: translateX(60px);
   }
   100% {
-    transform:translateX(0px);
+    transform: translateX(0px);
   }
 }
 @keyframes snowflakes-fade {
@@ -177,76 +177,80 @@ export default {
     opacity: 1;
   }
   100% {
-    opacity:0;
+    opacity: 0;
   }
 }
 .snowflake {
-  position:absolute;
+  position: absolute;
   opacity: 1;
-  top:-15%;
-  z-index:9999;
-  -webkit-user-select:none;
-  -moz-user-select:none;
-  -ms-user-select:none;
-  user-select:none;
+  top: -60%;
+  z-index: 9999;
   cursor: default;
+
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
   -webkit-animation-name: snowflakes-fall, snowflakes-shake, snowflakes-fade;
-  -webkit-animation-duration: 10s,3s,10.1s;
-  -webkit-animation-timing-function: linear,ease-in-out, linear;
-  -webkit-animation-iteration-count: infinite,infinite, infinite;
-  -webkit-animation-play-state: running,running, running;
+  -webkit-animation-duration: 13s, 3s, 13.1s;
+  -webkit-animation-timing-function: linear, ease-in-out, linear;
+  -webkit-animation-iteration-count: infinite, infinite, infinite;
+  -webkit-animation-play-state: running, running, running;
+
   animation-name: snowflakes-fall, snowflakes-shake, snowflakes-fade;
-  animation-duration: 10s,3s,10.1s;
-  animation-timing-function: linear,ease-in-out,linear;
-  animation-iteration-count: infinite,infinite,infinite;
-  animation-play-state: running,running,running;
+  animation-duration: 13s, 3s, 13.1s;
+  animation-timing-function: linear, ease-in-out, linear;
+  animation-iteration-count: infinite, infinite, infinite;
+  animation-play-state: running, running, running;
 }
 .snowflake:nth-of-type(0) {
   left: 1%;
-  -webkit-animation-delay: 0s,0s;
-  animation-delay: 0s,0s;
+  -webkit-animation-delay: 0s, 0s, 0s;
+  animation-delay: 0s, 0s, 0s;
 }
 .snowflake:nth-of-type(1) {
   left: 10%;
-  -webkit-animation-delay: 1s,1s;
-  animation-delay: 1s,1s;
+  -webkit-animation-delay: 1s, 1s, 1s;
+  animation-delay: 1s, 1s, 1s;
 }.snowflake:nth-of-type(2) {
   left: 20%;
-  -webkit-animation-delay: 6s,.5s;
-  animation-delay: 6s,.5s;
+  -webkit-animation-delay: 6s, .5s, 6s;
+  animation-delay: 6s, .5s, 6s;
 }
 .snowflake:nth-of-type(3) {
   left: 30%;
-  -webkit-animation-delay: 4s,2s;
-  animation-delay: 4s,2s;
+  -webkit-animation-delay: 4s, 2s, 4s;
+  animation-delay: 4s, 2s, 4s;
 }
 .snowflake:nth-of-type(4) {
-  left:40%;
-  -webkit-animation-delay: 2s,2s;
-  animation-delay: 2s,2s;
+  left: 40%;
+  -webkit-animation-delay: 2s, 2s, 2s;
+  animation-delay: 2s, 2s, 2s;
 }
 .snowflake:nth-of-type(5) {
   left: 50%;
-  -webkit-animation-delay: 8s,3s;
-  animation-delay:8s,3s;
+  -webkit-animation-delay: 8s, 3s, 8s;
+  animation-delay: 8s, 3s, 8s;
 }
 .snowflake:nth-of-type(6) {
-  left:60%; -webkit-animation-delay: 6s,2s;
-  animation-delay: 6s,2s;
+  left: 60%;
+  -webkit-animation-delay: 6s, 2s, 6s;
+  animation-delay: 6s, 2s, 6s;
 }
 .snowflake:nth-of-type(7) {
-  left:65%;
-  -webkit-animation-delay:2.5s,1s;
-  animation-delay:2.5s,1s;
+  left: 65%;
+  -webkit-animation-delay: 2.5s, 1s, 2.5s;
+  animation-delay: 2.5s, 1s, 2.5s;
 }
 .snowflake:nth-of-type(8) {
-  left:70%;
-  -webkit-animation-delay:1s,0s;
-  animation-delay:1s,0s;
+  left: 70%;
+  -webkit-animation-delay: 1s, 0s, 1s;
+  animation-delay: 1s, 0s, 1s;
 }
 .snowflake:nth-of-type(9) {
-  left:70%;
-  -webkit-animation-delay: 3s,1.5s;
-  animation-delay:3s,1.5s;
+  left: 70%;
+  -webkit-animation-delay: 3s, 1.5s, 3s;
+  animation-delay: 3s, 1.5s, 3s;
 }
 </style>
