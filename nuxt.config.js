9a6520098d5891870b8d646a894e7b9472e49a01
Kenny Heinonen <kenny_heinonen@hotmail.com>
2023-03-06 17:08:42 +0200

    Add icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,17 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', type: 'image/png', href: '/ninjamarttinen_logo.png' }
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '57x57', href: '/icons/apple-icon-57x57.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '60x60', href: '/icons/apple-icon-60x60.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '72x72', href: '/icons/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '76x76', href: '/icons/apple-icon-76x76.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '114x114', href: '/icons/apple-icon-114x114.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '120x120', href: '/icons/apple-icon-120x120.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '144x144', href: '/icons/apple-icon-144x144.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '152x152', href: '/icons/apple-icon-152x152.png' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', href: '/icons/apple-icon-180x180.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icons/favicon-96x96.png' }
     ]
   },
 
