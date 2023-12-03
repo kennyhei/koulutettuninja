1d59e9216f6f8a085499679fa6ca55a6b6f30b59
Kenny Heinonen <kenny_heinonen@hotmail.com>
2023-12-03 13:43:35 +0200

    Embed Timma booking calendar

--- pages/ajanvaraus.vue
+++ pages/ajanvaraus.vue
@@ -1,3 +1,34 @@
 <template>
-    <div>ajanvaraus</div>
+  <div id="ajanvaraus">
+    <div class="container">
+      <h2 class="title">Ajanvaraus</h2>
+    </div>
+    <!-- Embedded Timma booking calendar -->
+    <iframe
+      width="100%"
+      frameborder="0"
+      src="https://varaa.timma.fi/reservation/studioakkuokkala"
+      id="reservationIframe8714">
+    </iframe>
+  </div>
 </template>
+
+<script>
+export default {
+  mounted () {
+    // Resize iframe automatically
+    const script = document.createElement('script')
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/2.8.3/iframeResizer.min.js'
+    script.onload = () => {
+      iFrameResize({ checkOrigin: false }, '#reservationIframe8714')
+    }
+    document.head.appendChild(script)
+  }
+}
+</script>
+
+<style lang="scss">
+#ajanvaraus {
+  padding-top: 2.25em;
+}
+</style>
