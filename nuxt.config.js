6bf946ffb77c01d051871343a2c86338c43344aa
Kenny Heinonen <kenny_heinonen@hotmail.com>
2023-12-10 23:26:53 +0200

    Disable gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,7 +80,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/vue-gtag.js'
+    //'@/plugins/vue-gtag.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
